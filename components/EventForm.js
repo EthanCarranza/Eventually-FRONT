b639b78c27e9817b59f243c768e950692e0c7c1e
Ethan Carranza Serrano <ethan.carranza@outlook.es>
2025-07-09 12:11:31 +0200

    Revert "Refactor components and pages into subdirectories"
    This reverts commit 5a7cdcf21b1b4648db765740b0895e1623101168.

--- components/EventForm.js
+++ components/EventForm.js
@@ -2,29 +2,19 @@ export function EventForm({ isEdit = false, event = {} }) {
   return `
     <form id="create-event-form">
       <label for="event-title">Título del evento:</label>
-      <input type="text" id="event-title" value="${
-        event.title || ""
-      }" required />
+      <input type="text" id="event-title" value="${event.title || ''}" required />
       <label for="event-description">Descripción:</label>
-      <textarea id="event-description" required>${
-        event.description || ""
-      }</textarea>
+      <textarea id="event-description" required>${event.description || ''}</textarea>
       <label for="event-date">Fecha:</label>
-      <input type="datetime-local" id="event-date" value="${
-        event.date || ""
-      }" required />
+      <input type="datetime-local" id="event-date" value="${event.date || ''}" required />
       <label for="event-location">Ubicación:</label>
-      <input type="text" id="event-location" value="${
-        event.location || ""
-      }" required />
+      <input type="text" id="event-location" value="${event.location || ''}" required />
       <label for="event-image">Imagen del evento:</label>
       <input type="file" id="event-image" accept="image/*" style="display:none;" />
       <button id="upload-image-btn" type="button">Subir imagen</button>
       <div id="image-name-display">No se ha seleccionado ninguna imagen.</div>
       <div id="buttons-container">
-        <button type="submit" id="confirm-create-event-btn">${
-          isEdit ? "Actualizar evento" : "Crear evento"
-        }</button>
+        <button type="submit" id="confirm-create-event-btn">${isEdit ? 'Actualizar evento' : 'Crear evento'}</button>
         <button type="button" id="cancel-create-event-btn">Cancelar</button>
       </div>
     </form>
